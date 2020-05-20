56e8d8a526a990a5ded78b1686acd5bda3e86c0a
Elias Olofsson <tfy17eon@cs.umu.se>
2020-05-20 17:44:16 +0200

    End of day, have almost completed main structure.

--- ou5/is_connected.c
+++ ou5/is_connected.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>	
 #include <stdbool.h>	// Bool
 #include <errno.h>	// Better error handeling
@@ -6,9 +7,9 @@
 
 //#include "graph.h"
 
+#define BUFSIZE 300             /* Max 300 char per line */
 
-/*bool find_path(graph *g,node *src,node *dest)
-{}*/
+
 
 
 
@@ -44,57 +45,50 @@ bool line_is_comment(const char *s)
         return (i >= 0 && s[i] == '#');
 }
 
-
-
-
-
-
-
-#define BUFSIZE 300             /* Max 300 char per line */
-
-int main(int argc, char **argv)
+/**
+ * populate_graph() - Create and populate a new graph according to file.
+ * @filename: A string containing the file name of file to open.
+ *
+ * Returns: Pointer to a new and populated graph.
+ */
+void populate_graph(const char *filename) // CHANGE RETURN TYPE !!!!!!!!!!!!!!!
 {
 	FILE *in; 			// Pointer to input file
-	const char *filename;		// Input filename	
 	char line[BUFSIZE];     	// Input buffer
-
-	//int pos = 0; 		// Counter for file position
-	
-
-	// Verify input arguments
-	if (argc <= 1) {
-		fprintf(stderr, "Useage: filename\n\twhere filename is a file " 
-				"containing a directed graph structure.\n");
-		return -1;
-	} 
 	
 	// Try to open the input file
-	filename = argv[1];
 	in = fopen(filename, "r");
 	if (in == NULL) {
 		fprintf(stderr, "Couldn't open input file %s: %s\n", 
 			filename, strerror(errno));
-		return -1;
+		exit(EXIT_FAILURE);
 	} 
-	
+
 	// Repeat until end-of-file
 	bool first_line = true;
 	int nr_edges;
-	char node[40];
+	char node_str[40];
 	char neighbour[40];
 
+	int tmp = 0;
 	while (fgets(line, BUFSIZE, in) != NULL) {
 		if (line_is_blank(line) || line_is_comment(line)) {
                         // Ignore blank lines and comment lines.
                         continue;
                 }
-		
+
+		/* Extract integer from first line, create a new graph, then 
+		insert each following node pair. */
 		if (first_line){
 			sscanf(line,"%d", &nr_edges);
+			printf("Number of edges in graph: %d.\n", nr_edges);
+			//graph *g = graph_empty(nr_edges);
+			
 			first_line = false;
 		} else {
-			sscanf(line, "%40s %40s", node, neighbour);
-			
+			sscanf(line, "%40s %40s", node_str, neighbour);
+			printf("Entry %d: %s --> %s.\n", tmp, node_str, neighbour);
+			tmp++;
 			/* Insert nodes and edges intro graph.*/
 		}
 	}
@@ -103,16 +97,60 @@ int main(int argc, char **argv)
 	if (fclose(in)) {
                 fprintf(stderr, "Failed to close %s: %s\n", filename,
 			strerror(errno));
-                return -1;
+                exit(EXIT_FAILURE);
         }
+}
 
-	// Create graph
+/*
+bool find_path(graph *g,node *src,node *dest)
+{
 
-	// Main loop While
-		// Ask user for input 
+}
+*/
 
-		// Perform search 
+int main(int argc, char **argv)
+{
+	// Verify nr of input arguments.
+	if (argc <= 1) {
+		fprintf(stderr, "Useage: filename\n\twhere filename is a file " 
+				"containing a directed graph structure.\n");
+		return -1;
+	} 
+	
+	// Create and populate graph according to file.
+	const char *filename = argv[1];
+	populate_graph(filename);
+	//graph *g = populate_graph(filename);
+	
+	char str[255];
+	char src[40];
+	char dst[40];
+	bool running = true;
+	bool path_test;
 
+	while (running) {
+		printf("Enter origin and destination (quit to exit): ");
+		fgets(str, 255, stdin);
+		if (strncmp(str, "quit", 4) == 0) {
+			running = false;
+		} else {
+			sscanf(str, "%40s %40s", src, dst);
+			printf("Source: %s, Destination: %s.\n", src, dst);
 
+			// NEED TO CHECK USER INPUT !!!
+
+			path_test = find_path(g, src, dst);
+			if (path_test) {
+				printf();
+			} else {
+				printf();
+			}
+			
+
+			/* Perform search */
+		}
+	}
+
+	printf("Normal exit.\n");
 	return 0;
 }
