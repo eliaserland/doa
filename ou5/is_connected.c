266c16ff0f988e1223543f395e07e496af37498b
Elias Olofsson <tfy17eon@cs.umu.se>
2020-05-20 15:50:26 +0200

    Saving partial work.

--- ou5/is_connected.c
+++ ou5/is_connected.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <errno.h>
+#include <string.h>	
+#include <stdbool.h>	// Bool
+#include <errno.h>	// Better error handeling
+#include <ctype.h>	// isspace()
 
 //#include "graph.h"
 
@@ -9,11 +10,69 @@
 /*bool find_path(graph *g,node *src,node *dest)
 {}*/
 
+
+
+
+
+/* Return position of first non-whitespace character or -1 if only
+   white-space is found. */
+int first_non_white_space(const char *s)
+{
+        int i = 0; // Start at first char.
+        // Advance until we hit EOL as long as we're loooking at white-space.
+        while (s[i] && isspace(s[i])) {
+                i++;
+        }
+        if (s[i]) {
+                return i; // Return position of found a non-white-space char.
+        } else {
+                return -1; // Return fail.
+        }
+}
+
+/* Return position of last non-whitespace character or -1 if only
+   white-space is found. */
+int last_non_white_space(const char *s)
+{
+        // Start at last char.
+        int i = strlen(s) - 1;
+        // Move back until we hit beginning-of-line as long as we're
+        // loooking at white-space.
+        while (i >= 0 && isspace(s[i])) {
+                i--;
+        }
+        if (i >= 0) {
+                return i; // Return position of found a non-white-space char.
+        } else {
+                return -1; // Return fail.
+        }
+}
+
+/* Return true if s only contains whitespace */
+bool line_is_blank(const char *s)
+{
+        // Line is blank if it only contained white-space chars.
+        return first_non_white_space(s) < 0;
+}
+
+/* Return true if s is a comment line, i.e. first non-whitespc char is '#' */
+bool line_is_comment(const char *s)
+{
+        int i = first_non_white_space(s);
+        return (i >= 0 && s[i] == '#');
+}
+
+
+#define BUFSIZE 300             /* Max 300 char per line */
+
 int main(int argc, char **argv)
 {
-	FILE *in; 		// Pointer to input file
+	FILE *in; 			// Pointer to input file
+	const char *filename;		// Input filename	
+	char line[BUFSIZE];     	// Input buffer
+
 	//int pos = 0; 		// Counter for file position
-	const char *filename;		
+	
 
 	// Verify input arguments
 	if (argc <= 1) {
@@ -32,9 +91,22 @@ int main(int argc, char **argv)
 	} 
 	
 	// Repeat until end-of-file
+	while (fgets(line, BUFSIZE, in) != NULL) {
+		if (line_is_blank(line) || line_is_comment(line)) {
+                        // Ignore blank lines and comment lines.
+                        continue;
+                }
+
+		printf("%s",line);
+		/*const char *node;
+		const char *neighbour;
+		int beg = first_non_white_space(line);
+		while () {
+
+		}*/
 
 
-
+	}
 
 	// Close file
 	if (fclose(in)) {
